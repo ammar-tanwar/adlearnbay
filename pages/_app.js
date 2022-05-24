d0df781c007948df7c047a318ccb31c9e4e0c6a0
ammar.tanwar <ammartanwar123@gmail.com>
2022-05-24 14:49:59 +0530

    All Code

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import Navbar from "../components/module/Navbar/Navbar";
+import Footer from "../components/module/Footer/Footer";
+
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      {/* <Header /> */}
+      <Component {...pageProps} />
+      <Footer />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
