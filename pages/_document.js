4aac273977abae97417dea1a3d18b975bf1feb01
ammar.tanwar <ammartanwar123@gmail.com>
2022-06-04 13:27:59 +0530

    meta

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
+        <meta name="facebook-domain-verification" content="wr3mtenryw08ggdtdc1iwxqdqi5zma" />
         <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
