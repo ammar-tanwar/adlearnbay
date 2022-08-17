112dbe452442f36235c1cb7a7f3a19aa1374874d
Rohit <rajr25859@gmail.com>
2022-08-17 16:54:13 +0530

    push privious code

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script  from "next/script"
-
 
 export default function Document() {
   return (
@@ -25,15 +23,16 @@ export default function Document() {
           content="xzlsit1skwzpjhylrt0y96nn3nbksu"
         />
         <script src="https://www.googleoptimize.com/optimize.js?id=OPT-NQHBZ7H"></script>
-        <Script strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({‘gtm.start’:new Date().getTime(),event:‘gtm.js’});
-            var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!=‘dataLayer’?‘&l=‘+l:‘’;j.async=true;j.src=
-              ’https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,‘script’,‘dataLayer’,‘GTM-NN8XWH8’);`,
+
+        <script 
+         dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NN8XWH8');`,
           }}
-        > </Script>
+        />
       </Head>
       <body>
         <noscript
