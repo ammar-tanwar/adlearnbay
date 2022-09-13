adbda499cdb1adb98c63d2b04e02d24b65045e57
Rohit <rajr25859@gmail.com>
2022-09-13 18:00:03 +0530

    add gtm script im document

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import Script from "next/script";
 export default function Document() {
   return (
     <Html>
@@ -25,6 +25,25 @@ export default function Document() {
         />
         <script src="https://www.googleoptimize.com/optimize.js?id=OPT-NQHBZ7H"></script>
 
+
+        <script 
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){
+            w[l]=w[l]||[];
+            w[l].push({‘gtm.start’:new Date().getTime(),event:‘gtm.js’});
+            var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),
+            dl=l!=‘dataLayer’?‘&l=‘+l:‘’;
+            j.async=true;
+            j.src=’https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })
+          (window,document,‘script’,‘dataLayer’,‘GTM-MQV7XJC’);
+
+      ` }} />
+         
+
         {/* <script 
          dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -35,7 +54,12 @@ export default function Document() {
           }}
         /> */}
       </Head>
-      <body>
+      <body>  
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MQV7XJC" height="0" width="0" style={{display:"none",visibility:"hidden"}}>
+          </iframe>
+        </noscript>
+
         {/* <noscript
           dangerouslySetInnerHTML={{
             __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NN8XWH8"
