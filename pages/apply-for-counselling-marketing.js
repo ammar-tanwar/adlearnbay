b26d579bef1354ef462904f33314564b8517398e
Rohit <rajr25859@gmail.com>
2022-10-10 18:31:57 +0530

    design change in marketing

--- pages/apply-for-counselling-marketing.js
+++ pages/apply-for-counselling-marketing.js
@@ -4,10 +4,10 @@ import React from 'react'
 import Navbar from "../Components/NavbarA/Navbar";
 import Footer from "../Components/FooterA/Footer";
 import Form from "../Components/Form/Form";
-
+import Image from 'next/image';
 function Counselling() {
   return (
-    <div className={styles.container}>
+    <div className={styles.container1}>
       <Head>
         <title>Learnbay Courses</title>
         <meta name="description" content="Learnbay Courses" />
@@ -52,11 +52,36 @@ function Counselling() {
         />
       </Head>
       <Navbar radio={true} />
-      <div className={styles.formq}>
-        <div className={styles.forml}></div>
-        <div className={styles.formr}>
+      <div className={styles.formq1}>
+
+      <div className={styles.formr}>
           <Form radio={true} />
         </div>
+        <div className={styles.forml}>
+          <Image
+            src="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/home-page-header.webp"
+            width="870"
+            height="666"
+            layout="intrinsic"
+            alt="data science course in bangalore"
+          />
+          <div className={styles.info}>
+            <div className={styles.left}>
+              <h5>1:1</h5>
+              <p>Dedicated Mentorship</p>
+            </div>
+            <div className={styles.middle}>
+              <h5>100%</h5>
+              <p>Assured Interview</p>
+            </div>
+            <div className={styles.right}>
+              <h5>7+</h5>
+              <p>Domain Electives</p>
+            </div>
+          </div>
+
+        </div>
+        
       </div>
       <Footer />
     </div>
