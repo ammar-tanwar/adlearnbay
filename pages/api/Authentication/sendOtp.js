9e8c0d14d6df6c4712af78ce01373716cb08e863
ammar-tanwar <ammartanwar123@gmail.com>
2022-12-02 17:55:42 +0530

    otp change

--- pages/api/Authentication/sendOtp.js
+++ pages/api/Authentication/sendOtp.js
@@ -1,5 +1,5 @@
 import { connectToDatabase } from "../../../lib/mongodb";
-const axios = require('axios');
+const axios = require("axios");
 
 export default async function handler(req, res) {
   const { db } = await connectToDatabase();
@@ -8,20 +8,22 @@ export default async function handler(req, res) {
     let { mobileNumber } = req.body;
     // console.log(mobileNumber, "request api");
     var otp = Math.floor(1000 + Math.random() * 9000);
-    // console.log(otp);
+    console.log(otp);
     let addDateTime = new Date();
     addDateTime = addDateTime.toUTCString();
 
-    const MTALKZ_SEND_SMS_POST_API_URL = "http://msg.mtalkz.com/V2/http-api-post.php"
-    const MTALKZ_SEND_SMS_API_KEY = "WNlxSnQ0kz1DktWp"
-    const MTALKZ_SEND_SMS_SENDERID = "MTAMOI"
-    const message = `Your OTP- One Time Password is ${otp}  to authenticate your login with Learnbay Powered By mTalkz`
+    const MTALKZ_SEND_SMS_POST_API_URL = "http://msgn.mtalkz.com/api";
+    const MTALKZ_SEND_SMS_API_KEY = "n8XHGAd5oXn4J1eh";
+    const MTALKZ_SEND_SMS_SENDERID = "LEABAY";
+    const message = `${otp} is your OTP for registration. Use this for phone verification. Do not share this OTP for security reasons. Thanks, Learnbay.`;
 
     let filter = /^[0-9]{10}$/;
     if (filter.test(mobileNumber)) {
-      let data = await db.collection("otp").findOne({ "mobileNumber": mobileNumber });
+      let data = await db
+        .collection("otp")
+        .findOne({ mobileNumber: mobileNumber });
       if (data !== null) {
-        const mobileNumber1 = data.mobileNumber
+        const mobileNumber1 = data.mobileNumber;
         if (mobileNumber1 === mobileNumber) {
           const filter = { mobileNumber: mobileNumber };
           const updateDoc = {
@@ -33,55 +35,64 @@ export default async function handler(req, res) {
           };
           axios
             .post(MTALKZ_SEND_SMS_POST_API_URL, {
-              "apikey": MTALKZ_SEND_SMS_API_KEY,
-              "senderid": MTALKZ_SEND_SMS_SENDERID,
-              "number": mobileNumber,
-              "message": message,
-              "format": "json"
+              apikey: MTALKZ_SEND_SMS_API_KEY,
+              senderid: MTALKZ_SEND_SMS_SENDERID,
+              number: mobileNumber,
+              message: message,
+              format: "json",
             })
-            .then(resp => {
-              let data = resp.data
+            .then((resp) => {
+              let data = resp.data;
+              console.log(data);
               let otpData = db.collection("otp").updateMany(filter, updateDoc);
-              res.status(200).json({ msg: "OTP Sent Successfully", userStatus: "Already Exist" });
+              res.status(200).json({
+                msg: "OTP Sent Successfully",
+                userStatus: "Already Exist",
+              });
             })
-            .catch(error => {
+            .catch((error) => {
               res.status(200).json({ msg: " OTP Sending Failed Through API" });
               console.error(error);
             });
         } else {
-          res.status(200).json({ msg: "Mobile Number is Not Match from DataBase" });
+          res
+            .status(200)
+            .json({ msg: "Mobile Number is Not Match from DataBase" });
         }
       } else {
         axios
           .post(MTALKZ_SEND_SMS_POST_API_URL, {
-            "apikey": MTALKZ_SEND_SMS_API_KEY,
-            "senderid": MTALKZ_SEND_SMS_SENDERID,
-            "number": mobileNumber,
-            "message": message,
-            "format": "json"
+            apikey: MTALKZ_SEND_SMS_API_KEY,
+            senderid: MTALKZ_SEND_SMS_SENDERID,
+            number: mobileNumber,
+            message: message,
+            format: "json",
           })
-          .then(resp => {
-            let data = resp.data
+          .then((resp) => {
+            let data = resp.data;
+            console.log(data);
+
             let otpData = db.collection("otp").insertOne({
               mobileNumber: mobileNumber,
               otp: otp,
               isActive: {
                 type: Number,
-                default: 0
+                default: 0,
               },
               messageTemplate: message,
               addDateTime: addDateTime,
             });
-            res.status(200).json({ msg: "OTP Sent Successfully", userStatus: 'New User' });
+            res
+              .status(200)
+              .json({ msg: "OTP Sent Successfully", userStatus: "New User" });
           })
-          .catch(error => {
+          .catch((error) => {
             res.status(200).json({ msg: " OTP Sending Failed Through API" });
             console.error(error);
           });
       }
-    }
-    else {
-      res.status(200).json({msg: "Invalid Phone Number" });
+    } else {
+      res.status(200).json({ msg: "Invalid Phone Number" });
     }
   }
 }
