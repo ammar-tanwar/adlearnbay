c742aeca218cfd07de0e81f74735cdf5d2ad02b0
Rohit <rajr25859@gmail.com>
2022-12-29 18:38:41 +0530

    performance working pages

--- next.config.js
+++ next.config.js
@@ -13,6 +13,15 @@ module.exports = {
   },
 };
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
+})
+
+module.exports = withBundleAnalyzer({
+  env: {
+      NEXT_PUBLIC_ENV: 'PRODUCTION', //your next configs goes here
+  },
+})
 
 
 
