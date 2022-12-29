22a48f24316f3fd0fbc4b12ce33320b20ab5b05e
ammar-tanwar <ammartanwar123@gmail.com>
2022-12-30 00:42:06 +0530

    resolved

--- next.config.js
+++ next.config.js
@@ -13,15 +13,15 @@ module.exports = {
   },
 };
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true'
-})
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true'
+// })
 
-module.exports = withBundleAnalyzer({
-  env: {
-      NEXT_PUBLIC_ENV: 'PRODUCTION', //your next configs goes here
-  },
-})
+// module.exports = withBundleAnalyzer({
+//   env: {
+//       NEXT_PUBLIC_ENV: 'PRODUCTION', //your next configs goes here
+//   },
+// })
 
 
 
