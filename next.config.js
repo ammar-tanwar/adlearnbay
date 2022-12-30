0f8742a17ac98d62aea42b19a0c971381bbb00cb
Rohit <rajr25859@gmail.com>
2022-12-30 10:51:41 +0530

    remove next-config file nxt-build cod e

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-  "analyze": "ANALYZE=true next build.",
 }
 
 module.exports = {
@@ -12,16 +12,6 @@ module.exports = {
     domains: ["learnbay-wb.s3.ap-south-1.amazonaws.com"],
   },
 };
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true'
-})
-
-module.exports = withBundleAnalyzer({
-  env: {
-      NEXT_PUBLIC_ENV: 'PRODUCTION', //your next configs goes here
-  },
-})
 
 
 
