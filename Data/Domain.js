37264c79b1f6254b2b4e26d3e9277aeade3c4e8f
ammar-tanwar <ammartanwar123@gmail.com>
2023-03-20 18:36:47 +0530

    image change

--- Data/Domain.js
+++ Data/Domain.js
@@ -637,7 +637,7 @@ export const MarketingData = [
       h1: "We only make promises",
       h2: "we can keep",
       h2p: "Get assured interview calls from top recruiters",
-      hSRC: "https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/hiring-banking.png",
+      hSRC: "https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/course.learnbay.co/HR-Logos.png",
       rewHead: "Accelerating countless careers to the next level",
     },
     syllabusDesc:
@@ -928,7 +928,7 @@ export const SalesData = [
       h1: "We only make promises",
       h2: "we can keep",
       h2p: "Get assured interview calls from top recruiters",
-      hSRC: "https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/hiring-banking.png",
+      hSRC: "https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/course.learnbay.co/HR-Logos.png",
       rewHead: "Accelerating countless careers to the next level",
     },
     syllabusDesc:
