cb5649cc9f4bd5c310f0a2d412028cb8cf09c8b0
ammar-tanwar <ammartanwar123@gmail.com>
2023-05-02 13:17:21 +0530

    tawk to integrate

--- pages/s2-data-science.js
+++ pages/s2-data-science.js
@@ -86,7 +86,23 @@ export default function Home() {
             x.parentNode.insertBefore(s, x);`,
           }}
         /> */}
-        <script src="//code.tidio.co/mgl99n6cbvvabah9x8jj98no83kdozym.js" async></script>
+        {/* <script src="//code.tidio.co/mgl99n6cbvvabah9x8jj98no83kdozym.js" async></script> */}
+<script
+          dangerouslySetInnerHTML={{
+            __html: `
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6229dde31ffac05b1d7de6c4/1ftpolgri';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+`,
+          }}
+          />
+
       </Head>
       <main>
         {" "}
