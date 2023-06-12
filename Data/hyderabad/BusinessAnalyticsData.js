8b6b86ad279068e88267016168da40dd25ae70c6
ammar-tanwar <ammartanwar123@gmail.com>
2023-06-12 12:19:33 +0530

    new batch details

--- Data/hyderabad/BusinessAnalyticsData.js
+++ Data/hyderabad/BusinessAnalyticsData.js
@@ -310,12 +310,12 @@ export const BusinessAnalyticsCourseData = [
     Batch: [
       {
         Module0: {
-          title: "Weekday Evening Batch :- (Mon-Fri)",
+          title: "Weekday Morning Batch :- (Mon-Fri)",
           content: [
             {
               chap: {
                 title: "Batch 1 :- Date And time ",
-                desc: ["Date : 9th June 2023", "Time : 8 PM to 10 PM"],
+                desc: ["Date : 23rd June 2023", "Time : 8 AM to 10 AM"],
               },
             },
           ],
@@ -325,12 +325,12 @@ export const BusinessAnalyticsCourseData = [
       },
       {
         Module0: {
-          title: "Weekend Afternoon Batch :- (Sat-Sun)",
+          title: "Weekend Morning Batch :- (Sat-Sun)",
           content: [
             {
               chap: {
                 title: "Batch 1 :- Date And time ",
-                desc: ["Date : 11th June 2023", "Time : 12:30 PM to 4 PM"],
+                desc: ["Date : 25th June 2023", "Time : 8:30 AM to 12 PM"],
               },
             },
           ],
