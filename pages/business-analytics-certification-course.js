1f39fbd3831a1d074e47e18e09e646d301f4099b
ammar-tanwar <ammartanwar123@gmail.com>
2023-07-24 17:42:52 +0530

    organic chat bot implement

--- pages/business-analytics-certification-course.js
+++ pages/business-analytics-certification-course.js
@@ -27,6 +27,7 @@ export default function Home() {
     setPopups(true);
   };
   return (
+<>
     <div>
       <Head>
         <title>Business Analytics Certification Program</title>
@@ -42,7 +43,7 @@ export default function Home() {
           rel="canonical"
           href="https://www.learnbay.co/business-analytics-certification-course"
         />
-        <script
+                {/* <script
           dangerouslySetInnerHTML={{
             __html: `var url = 'https://wati-integration-service.clare.ai/ShopifyWidget/shopifyWidget.js?77928';
         var s = document.createElement('script');
@@ -80,7 +81,7 @@ export default function Home() {
         var x = document.getElementsByTagName('script')[0];
         x.parentNode.insertBefore(s, x);`,
           }}
-        />
+        /> */}
         {/* <script
           src="//code.tidio.co/mgl99n6cbvvabah9x8jj98no83kdozym.js"
           async
@@ -220,5 +221,24 @@ export default function Home() {
         <Footer />
       </main>
     </div>
+    <script
+        dangerouslySetInnerHTML={{
+          __html: `(function (botId) {
+            var s = document.createElement("script");
+            s.async = true;
+            s.type = 'text/javascript';
+            s.src = "https://app.chat360.io/widget/chatbox/common_scripts/script.js";
+            s.onload = function () {
+              window.loadChat360Bot(botId);
+            };
+            s.onerror = function (err) {
+              console.error(err);
+            };
+            document.body.appendChild(s);
+          })("d22aede5-fcaf-423d-ab4c-2e49782f9cc2");
+      `,
+        }}
+      />
+    </>
   );
 }
