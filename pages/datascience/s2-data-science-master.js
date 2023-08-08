7b071ce6ca2317de99069ff137d55b74ab2561d5
ammar-tanwar <ammartanwar123@gmail.com>
2023-08-08 16:57:29 +0530

    new master program

---
+++ pages/datascience/s2-data-science-master.js
@@ -0,0 +1,170 @@
+import Head from "next/head";
+import React, { useState } from "react";
+import Popup from "../../Components/Popup/Popup";
+import Form from "../../Components/Form/Form";
+import Navbar from "../../Components/CoursePage/Navbar/Navbar";
+
+import SliderTab from "../../Components/CoursePage/SliderTab/SliderTabs";
+import HeroSection from "../../Components/LightTheme/FirstSection/HeroSection";
+import Testimonial from "../../Components/LightTheme/SixthSection/Testimonial";
+import SecondSection from "../../Components/LightTheme/SecondSection/SecondSection";
+import ThirdSection from "../../Components/LightTheme/ThirdSection/ThirdSection";
+import FourthSection from "../../Components/LightTheme/FourthSection/FourthSection";
+import SeventhSection from "../../Components/LightTheme/SeventhSection/SeventhSection";
+import EightSection from "../../Components/LightTheme/EightSection/EightSection";
+import SyllabusSection from "../../Components/LightTheme/SyllabusSection/SyllabusSection";
+
+export default function Home() {
+  const [popups, setPopups] = useState(false);
+
+  const popupShow = () => {
+    setPopups(true);
+  };
+  return (
+    <>
+      <div>
+        <Head>
+          <title>Online Data Science and AI Master Program</title>
+          <meta
+            name="description"
+            content="Want to earn salary hike of 250% ? Our Data Science and AI Master Program 
+ will boost salary. Apply Now!"
+          />
+
+          <meta
+            name="Keywords"
+            content="Data Science and AI Master Program, Data Science and AI Master Program for working professional, Data Science and AI Master Program fee, Data Science and AI Master Program duration, Data Science and AI Master Program syllabus, data science online course"
+          />
+
+          <link
+            rel="icon"
+            href="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/Learnbay-Favicon-L.png"
+          />
+          {/* <script
+          dangerouslySetInnerHTML={{
+            __html: `var url = 'https://wati-integration-service.clare.ai/ShopifyWidget/shopifyWidget.js?77928';
+            var s = document.createElement('script');
+            s.type = 'text/javascript';
+            s.async = true;
+            s.src = url;
+            var options = {
+          "enabled":true,
+          "chatButtonSetting":{
+              "backgroundColor":"#4dc247",
+              "ctaText":"",
+              "borderRadius":"10",
+              "marginLeft":"0",
+              "marginBottom":"20",
+              "marginRight":"20",
+              "position":"right",
+              "zIndex":"22222222 !important",
+          },
+          "brandSetting":{
+              "brandName":"Learnbay",
+              "brandSubTitle":"The Learner's Path",
+              "brandImg":"https://course.learnbay.co/Learnbay-Favicon-L.png",
+              "welcomeText":"Hi there! How can I help you?",
+ "messageText":"I am looking for course, and free career counselling session.",
+              "backgroundColor":"#0a5f54",
+              "ctaText":"Start Chat",
+              "borderRadius":"25",
+              "autoShow":false,
+              "phoneNumber":"+918123327570"
+          }
+        };
+            s.onload = function() {
+                CreateWhatsappChatWidget(options);
+            };
+            var x = document.getElementsByTagName('script')[0];
+            x.parentNode.insertBefore(s, x);`,
+          }}
+        /> */}
+          {/* <script
+          src="//code.tidio.co/mgl99n6cbvvabah9x8jj98no83kdozym.js"
+          async
+        ></script> */}
+        </Head>
+        <main>
+          {" "}
+          <Navbar
+            popup={true}
+            brochureButton={true}
+            formotp={true}
+            s2dataScience={true}
+          />
+          <Popup trigger={popups} setTrigger={setPopups} className="popupModal">
+            <div className="leftPopup">
+              <div className="whiteP" />
+            </div>
+            <div className="RightPopup">
+              <h5>Apply For Counselling</h5>
+              <Form popup={true} setTrigger={setPopups} />
+            </div>
+          </Popup>
+          <HeroSection
+            formotp={true}
+            s2dataScience={true}
+            deskTopPara="Become a master without master’s degree"
+            h1="Discover new horizons with"
+            hSpan="Data Science and AI Master Program"
+            para="Successfully transition careers through our courses & real-time capstone projects. 
+          All while making your previous work experience count!"
+            sideImg="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/ALight/data+science+and+ai.jpg"
+          />
+          <SecondSection
+            p1="IBM Certified"
+            p11="Capstone Projects"
+            p2="5-star"
+            p22="Industry-standard Training"
+            p3="3-year"
+            p33="Subscription Model"
+            p4="1:1 Support"
+            p44="Dedicated Program"
+          />
+          <ThirdSection formotp={true} s2dataScience={true} />
+          <FourthSection
+            h1="Program Highlights"
+            img1="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/ALight/online+interactive+sessionn.jpg"
+            img2="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/ALight/weekend+and+weekday+batch.jpg"
+            img3="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/ALight/extra+dout+clearance+sessionn.jpg"
+            img4="https://learnbay-wb.s3.ap-south-1.amazonaws.com/main/ALight/Multiple+Domain+Selection+Options.jpg"
+          />
+          <SyllabusSection
+            formotp={true}
+            s2dataScience={true}
+            desc="Learnbay offers live, interactive online sessions with professionals from top MNCs. Our Data Science and AI Master Program feature practical training with real-time projects and case studies."
+            Hours="500+ Hours"
+            Projects="16+ projects"
+            Module0="Preparatory Classes (Programming + Maths)"
+            Module1="Python for Data Science"
+            Module2="Statistics and Machine Learning"
+            Module3="Data Science and AI Tools"
+            Module4="Deployment (AWS+Azure)"
+          />
+          <SliderTab />
+          <Testimonial formotp={true} s2dataScience={true} />
+          <SeventhSection />
+          <EightSection formotp={true} s2dataScience={true} />
+        </main>
+      </div>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `(function (botId) {
+            var s = document.createElement("script");
+            s.async = true;
+            s.type = 'text/javascript';
+            s.src = "https://app.chat360.io/widget/chatbox/common_scripts/script.js";
+            s.onload = function () {
+              window.loadChat360Bot(botId);
+            };
+            s.onerror = function (err) {
+              console.error(err);
+            };
+            document.body.appendChild(s);
+          })("d22aede5-fcaf-423d-ab4c-2e49782f9cc2");
+      `,
+        }}
+      />
+    </>
+  );
+}
